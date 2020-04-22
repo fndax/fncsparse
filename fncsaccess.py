708881eb7db9bb223c76a97740f0f95705c2f027
Dax <23448388+fndax@users.noreply.github.com>
2020-04-22 07:14:12 +0000

    Fix all pylint warnings

--- fncsaccess.py
+++ fncsaccess.py
@@ -1,26 +1,39 @@
+"""Creates an IRC bot, connects it to IRC, and then runs `CHANSERV ACCESS
+CHANNELNAME LIST` on all channels in a file channels-in in the current
+directory, then pulls a list of people on the ACL.
+
+Needs a file channels-in in the current directory, which it ingests to provide
+a list of channels.
+
+Writes output to channels-out-acls in the current directory."""
+
+from typing import Dict
+import re
 import asyncio
 
 from irctokens import build, Line
+from ircstates.numerics import RPL_WELCOME
 from ircrobots import Bot as BaseBot
 from ircrobots import Server as BaseServer
 from ircrobots import ConnectionParams
-from ircstates.numerics import *
-from ircrobots.matching import Response, ANY, Nickname, Regex, ResponseOr, Formatless
+from ircrobots.matching import Response, ANY, Nickname, Regex, ResponseOr
 
-import re
-from typing import Dict
-
-with open("channels-in") as f:
-    CHANLIST = f.read().splitlines()
+with open("channels-in") as channels_in:
+    CHANLIST = channels_in.read().splitlines()
 
 CHANSERV = Nickname("ChanServ")
 
-INFO_ENDOFACCESS = Response("NOTICE", [ANY, Regex(r"End of .* FLAGS listing.")], source=CHANSERV)
-INFO_ACLENTRY    = Response("NOTICE", [ANY, Regex(r"^[0-9]+\s+[^\s]+\s+")], source=CHANSERV)
-INFO_NOTREG      = Response("NOTICE", [ANY, Regex(r"is not registered\.")], source=CHANSERV)
-INFO_NOTAUTHED   = Response("NOTICE", [ANY, Regex(r"You are not authorized to perform this operation.")], source=CHANSERV)
+INFO_ENDOFACCESS = Response("NOTICE", [ANY, Regex(r"End of .* FLAGS listing.")],
+                            source=CHANSERV)
+INFO_ACLENTRY = Response("NOTICE", [ANY, Regex(r"^[0-9]+\s+[^\s]+\s+")],
+                         source=CHANSERV)
+INFO_NOTREG = Response("NOTICE", [ANY, Regex(r"is not registered\.")],
+                       source=CHANSERV)
+INFO_NOTAUTHED = Response("NOTICE", [ANY, Regex(r"You are not authorized to" \
+    "perform this operation.")], source=CHANSERV)
 
 class Server(BaseServer):
+    """Overridden Server class handling line reading and writing"""
     async def line_read(self, line: Line):
         print(f"{self.name} < {line.format()}")
         if line.command == RPL_WELCOME:
@@ -29,11 +42,18 @@ class Server(BaseServer):
                 await self.send(build("CHANSERV", ["ACCESS", channel, "LIST"]))
 
                 while True:
-                    line = await self.wait_for(ResponseOr(INFO_ACLENTRY, INFO_ENDOFACCESS, INFO_NOTREG, INFO_NOTAUTHED))
-                    aclmatch = re.match(r"^[0-9]+\s+([^\s]+)\s+", line.params[1])
+                    line = await self.wait_for(ResponseOr(INFO_ACLENTRY,
+                                                          INFO_ENDOFACCESS,
+                                                          INFO_NOTREG,
+                                                          INFO_NOTAUTHED))
+
+                    aclmatch = re.match(r"^[0-9]+\s+([^\s]+)\s+",
+                                        line.params[1])
+
                     if aclmatch and aclmatch.group(1) == "freenode-staff":
                         continue
-                    elif aclmatch and channel in acls:
+
+                    if aclmatch and channel in acls:
                         acls[channel] += f", {aclmatch.group(1)}"
                     elif aclmatch:
                         acls[channel] = aclmatch.group(1)
@@ -41,20 +61,27 @@ class Server(BaseServer):
                         if not channel in acls:
                             acls[channel] = "N/A"
                         break
+
             print(acls)
-            with open("channels-out-acls", "w") as f:
-                [f.write(f"{k}\t{v}\n") for k,v in acls.items()]
+            with open("channels-out-acls", "w") as channels_out:
+                for channel, acl in acls.items():
+                    channels_out.write(f"{channel}\t{acl}\n")
 
     async def line_send(self, line: Line):
         print(f"{self.name} > {line.format()}")
 
 class Bot(BaseBot):
+    """Basic Bot subclass"""
     def create_server(self, name: str):
         return Server(self, name)
 
 async def main():
+    """Add something that is almost but not quite freenode to the bot's
+    connection parameters, then start it up."""
+
     bot = Bot()
-    params = ConnectionParams(nickname="daxbot", host="chat.freenode.net", port=6697, tls=True, realname="dax's Magical Robot (using jess's ircrobots <3)")
+    params = ConnectionParams(nickname="testbot", host="chat.freenode.invalid",
+                              port=6697, tls=True, realname="Test Robot")
     await bot.add_server("freenode", params)
 
     await bot.run()
--- fncsmlock.py
+++ fncsmlock.py
@@ -1,52 +1,72 @@
+"""Creates an IRC bot, connects it to IRC, and then runs `CHANSERV INFO
+CHANNELNAME` on all channels in a file channels-in in the current
+directory, then pulls the MLOCK (if set) from that.
+
+Needs a file channels-in in the current directory, which it ingests to provide
+a list of channels.
+
+Writes output to channels-out-mlock in the current directory."""
+
 import asyncio
 
 from irctokens import build, Line
+from ircstates.numerics import RPL_WELCOME
 from ircrobots import Bot as BaseBot
 from ircrobots import Server as BaseServer
 from ircrobots import ConnectionParams
-from ircstates.numerics import *
-from ircrobots.matching import Response, ANY, Nickname, Regex, ResponseOr, Formatless
+from ircrobots.matching import ANY, Nickname, Regex, Formatless
+from ircrobots.matching import Response, ResponseOr
 
 with open("channels-in") as f:
     CHANLIST = f.read().splitlines()
 
 CHANSERV = Nickname("ChanServ")
 
-INFO_ENDOFINFO = Response("NOTICE", [ANY, Formatless("*** End of Info ***")], source=CHANSERV)
-INFO_MLOCK     = Response("NOTICE", [ANY, Regex(r"Mode lock")], source=CHANSERV)
-INFO_NOTREG    = Response("NOTICE", [ANY, Regex(r"is not registered\.")], source=CHANSERV)
+INFO_ENDOFINFO = Response("NOTICE", [ANY, Formatless("*** End of Info ***")],
+                          source=CHANSERV)
+INFO_MLOCK = Response("NOTICE", [ANY, Regex(r"Mode lock")], source=CHANSERV)
+INFO_NOTREG = Response("NOTICE", [ANY, Regex(r"is not registered\.")],
+                       source=CHANSERV)
 
 class Server(BaseServer):
+    """Overridden Server class handling line reading and writing"""
     async def line_read(self, line: Line):
         print(f"{self.name} < {line.format()}")
         if line.command == RPL_WELCOME:
-            mlock = {}
+            mlocks = {}
             for channel in CHANLIST:
                 await self.send(build("CHANSERV", ["INFO", channel]))
 
                 while True:
-                    line = await self.wait_for(ResponseOr(INFO_MLOCK, INFO_ENDOFINFO, INFO_NOTREG))
+                    line = await self.wait_for(ResponseOr(INFO_MLOCK,
+                                                          INFO_ENDOFINFO,
+                                                          INFO_NOTREG))
                     if line.params[1].startswith("Mode lock"):
                         _, _2, modes = line.params[1].partition(": ")
-                        mlock[channel] = modes
+                        mlocks[channel] = modes
                     else:
-                        if not channel in mlock:
-                            mlock[channel] = "unset"
+                        if not channel in mlocks:
+                            mlocks[channel] = "unset"
                         break
-            print(mlock)
-            with open("channels-out", "w") as f:
-                [f.write(f"{k}\t{v}\n") for k,v in mlock.items()]
+            print(mlocks)
+            with open("channels-out-mlock", "w") as channels_out:
+                for channel, mlock in mlocks.items():
+                    channels_out.write(f"{channel}\t{mlock}\n")
 
     async def line_send(self, line: Line):
         print(f"{self.name} > {line.format()}")
 
 class Bot(BaseBot):
+    """Basic Bot subclass"""
     def create_server(self, name: str):
         return Server(self, name)
 
 async def main():
+    """Add something that is almost but not quite freenode to the bot's
+    connection parameters, then start it up."""
     bot = Bot()
-    params = ConnectionParams(nickname="testbot", host="chat.freenode.invalid", port=6697, tls=True, realname="Test Magical Robot (using jess's ircrobots <3)")
+    params = ConnectionParams(nickname="testbot", host="chat.freenode.invalid",
+                              port=6697, tls=True, realname="Test Robot")
     await bot.add_server("freenode", params)
 
     await bot.run()
